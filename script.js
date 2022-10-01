312e6ff350ac80006b69bb4aa8fa0832c2152467
Solomon Edet <solospage2k@yahoo.com>
2022-10-01 21:57:44 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//  Create variables
+
+const profileMenu = document.getElementById("profileMenu");
+
+const sidebarActivity = document.getElementById("sidebarActivity");
+const moreLink = document.getElementById("showMoreLink");
+
+
+// Menu and Activity functionality
+
+function toggleMenu() {
+    profileMenu.classList.toggle("open-menu");
+}
+
+function toggleActivity() {
+    sidebarActivity.classList.toggle("open-activity");
+
+    if (sidebarActivity.classList.contains("open-activity")) {
+        moreLink.innerHTML = "Show less <b>-</b>";
+    } else {
+        moreLink.innerHTML = "Show more <b>+</b>";
+    }
+}
